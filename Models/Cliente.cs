478252ae36dc9cf34d3bb76eda06f26b3adef0e6
gramz <gramz@LAB3-CAT>
2026-05-04 08:51:33 -0600

    Agregamos Data Anotations al Cliente

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -4,9 +4,18 @@ namespace EjemploClientes.Models
 {
     public class Cliente
     {
-        
+        [Required(ErrorMessage = "El NIT es requerido")]
+        [StringLength(13, ErrorMessage = "El NIT debe tener 13 caractéres")]
         public string Nit { get; set; } = string.Empty;
+
+
+        [Required(ErrorMessage = "El Nombre es requerido")]
+        [Display(Name = "Nombre del Cliente")]
+        
         public string Nombre { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El apellido es requerido")]
+        [Display(Name = "Apellido del Cliente")]        
         public string Apellido { get; set; } = string.Empty;
         public int Edad {  get; set; }
     }
